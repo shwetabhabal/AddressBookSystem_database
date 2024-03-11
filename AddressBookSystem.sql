a3877eacf299a06a700af669a6082c9b1730aa08
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-11 17:56:03 +0530

    create address book table

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -1,1 +1,12 @@
 create database AddressBookSystem;
+use AddressBookSystem;
+create table addressBook(
+id int primary key auto_increment,
+firstName varchar(30),
+lastName varchar(30),
+address varchar(30),
+city varchar(30) default 'Mumbai',
+state varchar(30) default 'Maharashtra',
+phone bigint ,
+email varchar(50)
+);
